ae86cf1c1cdeba1de63dc63054a2a9cbc07d0a72
swakv <swathkv@gmail.com>
2020-11-03 17:43:01 +0800

    thx sid

---
+++ code/sid.py
@@ -0,0 +1,111 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers.recurrent import LSTM, GRU,SimpleRNN
+from tensorflow.keras.models import Model
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.embeddings import Embedding
+from keras.layers.normalization import BatchNormalization
+from keras.utils import np_utils
+from sklearn import preprocessing, decomposition, model_selection, metrics, pipeline
+from keras.layers import GlobalMaxPooling1D, Conv1D, MaxPooling1D, Flatten, Bidirectional, SpatialDropout1D
+from keras.preprocessing import sequence, text
+from keras.callbacks import EarlyStopping
+from transformers import TFBertModel,  BertConfig, BertTokenizerFast
+
+# Detect hardware, return appropriate distribution strategy
+try:
+    # TPU detection. No parameters necessary if TPU_NAME environment variable is
+    # set: this is always the case on Kaggle.
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+    print('Running on TPU ', tpu.master())
+except ValueError:
+    tpu = None
+
+if tpu:
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
+    strategy = tf.distribute.experimental.TPUStrategy(tpu)
+else:
+    # Default distribution strategy in Tensorflow. Works on CPU and single GPU.
+    strategy = tf.distribute.get_strategy()
+
+print("REPLICAS: ", strategy.num_replicas_in_sync)
+
+model_name = 'bert-base-multilingual-cased'
+
+config = BertConfig.from_pretrained(model_name)
+config.output_hidden_states = False
+tokenizer = BertTokenizerFast.from_pretrained(pretrained_model_name_or_path = model_name, config = config)
+transformer_model = TFBertModel.from_pretrained(model_name, config = config)
+
+train = pd.read_csv('/kaggle/input/dataset/train.csv')
+valid = pd.read_csv('/kaggle/input/dataset/val.csv')
+test = pd.read_csv('/kaggle/input/dataset/test.csv')
+
+#IMP DATA FOR CONFIG
+
+AUTO = tf.data.experimental.AUTOTUNE
+
+
+# Configuration
+EPOCHS = 3
+BATCH_SIZE = 16 * strategy.num_replicas_in_sync
+MAX_LEN = 168
+
+xtrain, xvalid, ytrain, yvalid = train_test_split(train.Comment.values, train.Emotion.values, 
+                                                  stratify=train.Emotion.values, 
+                                                  random_state=42, 
+                                                  test_size=0.2, shuffle=True)
+
+train['Comment'].apply(lambda x:len(str(x).split())).max()
+
+# using keras tokenizer here
+token = text.Tokenizer(num_words=None)
+max_len = 168
+
+token.fit_on_texts(list(xtrain) + list(xvalid))
+xtrain_seq = token.texts_to_sequences(xtrain)
+xvalid_seq = token.texts_to_sequences(xvalid)
+
+#zero pad the sequences
+xtrain_pad = sequence.pad_sequences(xtrain_seq, maxlen=max_len)
+xvalid_pad = sequence.pad_sequences(xvalid_seq, maxlen=max_len)
+
+word_index = token.word_index
+
+def build_model(transformer):
+    input_ids = Input(shape=(max_len,), dtype=tf.int32, name="input_ids")
+    sequence_output = transformer(input_ids)[0]
+    cls_token = sequence_output[:, 0, :]
+    out = Dense(7, activation='softmax')(cls_token)
+
+    model = Model(inputs=input_ids, outputs=out)
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    model.compile(Adam(lr=1e-5), loss=loss, metrics=['accuracy'])
+
+    return model
+
+metrics = [tf.keras.metrics.SparseCategoricalAccuracy('accuracy', dtype=tf.float32)]
+loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+with strategy.scope():
+    # A simpleRNN without any pretrained embeddings and one dense layer
+    # model = Sequential()
+    # model.add(Embedding(len(word_index) + 1,300,input_length=max_len))
+    # model.add(SimpleRNN(100))
+    # model.add(Dense(7, activation='softmax'))
+    # model.compile(loss=loss, optimizer='adam', metrics=['accuracy'])
+    model = build_model(transformer_model)
+    
+model.summary()
+
+
+model.fit(xtrain_pad, ytrain,epochs=10, batch_size=64*strategy.num_replicas_in_sync) #Multiplying by Strategy to run on TPU's
+
+model.evaluate(xvalid_pad, yvalid)
